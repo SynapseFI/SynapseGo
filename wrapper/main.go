5ea692865a79ddec57dd03a7ffdf89bc301f6426
Nathaniel Niosco <nathanielniosco@Nathaniels-MacBook-Pro.local>
2018-11-14 10:37:44 -0800

    refactor GET users for modularity

--- wrapper/main.go
+++ wrapper/main.go
@@ -8,6 +8,9 @@ import (
 	"net/http"
 )
 
+/********** GLOBAL VARIABLES **********/
+const _url = "http://uat-api.synapsefi.com/v3.1"
+
 /********** STRUCTS **********/
 
 // ClientCredentials structure of client object
@@ -27,58 +30,77 @@ func NewClient(gateway, ipAddress, userID string) ClientCredentials {
 }
 
 // GetUser GET method to GET user information
-// Confirm the correct type to return from function
-func GetUser(credentials ClientCredentials) map[string]interface{} {
-	_url := "http://uat-api.synapsefi.com/v3.1/users"
+// *CHECK* Confirm the correct type to return from function
+func GetUser(cred ClientCredentials) map[string]interface{} {
+	url := _url + "/users"
 
-	client := &http.Client{}
+	c := &http.Client{}
 
-	req := updateRequest(credentials, "GET", _url, nil)
+	req := updateRequest(cred, "GET", url, nil)
 
-	resp, err := client.Do(req)
+	resp := execRequest(c, req)
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	body := readResponse(resp)
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	var data interface{}
-	json.Unmarshal(body, &data)
-
-	jsonData, ok := data.(map[string]interface{})
-
-	if ok != false {
-		jsonData["id"] = credentials.userID
-	}
-
-	return jsonData
+	return jsonifyData(cred, body)
 }
 
 /********** HELPER FUNCTIONS **********/
 
-// sets client headers using client credentials
-func setHeaders(client ClientCredentials, request *http.Request) {
-	request.Header.Set("x-sp-gateway", client.gateway)
-	request.Header.Set("x-sp-user-ip", client.ipAddress)
-	request.Header.Set("x-sp-user", client.userID)
-	request.Header.Set("content-type", "application/json;charset=UTF-8")
-}
-
-// updates request headers with method, url, and body (if applicable)
-func updateRequest(client ClientCredentials, method, url string, body io.Reader) *http.Request {
-	req, err := http.NewRequest(method, url, body)
-
-	setHeaders(client, req)
+// executes request
+func execRequest(client *http.Client, request *http.Request) *http.Response {
+	response, err := client.Do(request)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	return req
+	return response
+}
+
+func jsonifyData(credentials ClientCredentials, responseBody []byte) map[string]interface{} {
+	var data interface{}
+	json.Unmarshal(responseBody, &data)
+
+	jsonData, isOK := data.(map[string]interface{})
+
+	// add userID as "id" to jsonData
+	if isOK != false {
+		jsonData["id"] = credentials.userID
+	}
+
+	return jsonData
+
+}
+
+func readResponse(response *http.Response) []byte {
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return body
+}
+
+// sets client headers using client credentials
+func setHeaders(credentials ClientCredentials, request *http.Request) {
+	request.Header.Set("x-sp-gateway", credentials.gateway)
+	request.Header.Set("x-sp-user-ip", credentials.ipAddress)
+	request.Header.Set("x-sp-user", credentials.userID)
+	request.Header.Set("content-type", "application/json;charset=UTF-8")
+}
+
+// updates request headers with method, url, and body (if applicable)
+func updateRequest(credentials ClientCredentials, method, url string, body io.Reader) *http.Request {
+	request, err := http.NewRequest(method, url, body)
+
+	setHeaders(credentials, request)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return request
 }
