429d652663e83f9988576e5a8548b7de1ab95595
Nathaniel Niosco <n.niosco@gmail.com>
2018-11-20 17:00:32 -0800

    remove unused code

--- wrapper/nodes.go
+++ wrapper/nodes.go
@@ -11,8 +11,6 @@ func (c *Client) GetAllNodes(queryParams ...map[string]interface{}) map[string]i
 		Get(nodesURL).
 		Query(queryString(queryParams)).
 		Set("x-sp-gateway", c.gateway).
-		// Set("x-sp-user-ip", c.ipAddress).
-		// Set("x-sp-user", c.userID).
 		EndBytes()
 
 	if res != nil && errs != nil {
