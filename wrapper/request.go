1e4a83b1f7d3e89fdc178be49b1c1db69674dc75
Nathaniel Niosco <nathan@synapsefi.com>
2018-11-27 12:04:46 -0800

    update request methods

--- wrapper/request.go
+++ wrapper/request.go
@@ -7,8 +7,40 @@ import (
 /********** GLOBAL VARIABLES **********/
 var request = gorequest.New()
 
-/********** METHODS **********/
-// func (c *Client, httpMethod string, data ...string) []byte {
+const get = "get"
+const post = "post"
+const patch = "patch"
 
-// 	return body
-// }
+/********** METHODS **********/
+
+func apiRequest(method, url string, data ...string) *gorequest.SuperAgent {
+	var req = gorequest.New()
+	req = setMethod(method, url)
+
+	switch len(data) {
+	case 1:
+		req = req.Send(data[0])
+
+	case 2:
+		req = req.Send(data[0]).Query(data[1])
+	}
+
+	return req
+}
+
+func setHeader() {
+
+}
+
+func setMethod(m, u string) *gorequest.SuperAgent {
+	switch m {
+	case "post":
+		return request.Post(u)
+
+	case "patch":
+		return request.Patch(u)
+
+	default:
+		return request.Get(u)
+	}
+}
