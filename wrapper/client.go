dabdb79efd73a0c74509e6d9b71705767f9d8b91
Nathaniel Niosco <nathan@synapsefi.com>
2018-11-28 19:22:03 -0800

    change name of client method

--- wrapper/client.go
+++ wrapper/client.go
@@ -16,9 +16,8 @@ type (
 
 /********** METHODS **********/
 
-// GenerateClient creates a client object
-// func GenerateClient(gateway, ipAddress, userID string, devMode ...bool) *Client {
-func GenerateClient(params interface{}) *Client {
+// NewClient creates a client object
+func NewClient(params interface{}) *Client {
 	p := params.(map[string]interface{})
 
 	if p["devMode"] == true {
