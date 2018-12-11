30b5e459ce8fef0cd3fe6e91074de22031203d43
Nathaniel Niosco <nathan@synapsefi.com>
2018-12-11 15:08:34 -0800

    refactor to return response objects from api calls

--- wrapper/users.go
+++ wrapper/users.go
@@ -1,5 +1,7 @@
 package wrapper
 
+import "github.com/mitchellh/mapstructure"
+
 /*********** GLOBAL VARIABLES ***********/
 
 /********** TYPES **********/
@@ -24,44 +26,44 @@ type (
 
 	// User represents a single user object
 	User struct {
-		AuthKey       string `json:"oauth_key"`
+		AuthKey       string
 		FullDehydrate bool
-		UserID        string `json:"_id"`
-		RefreshToken  string `json:"refresh_token"`
+		UserID        string `mapstructure:"_id"`
+		RefreshToken  string `mapstructure:"refresh_token"`
 		Response      interface{}
 		request       Request
 	}
 
 	// Users represents a collection of user objects
 	Users struct {
-		Limit      int64  `json:"limit"`
-		Page       int64  `json:"page"`
-		PageCount  int64  `json:"page_count"`
-		UsersCount int64  `json:"users_count"`
-		Users      []User `json:"users"`
+		Limit      int64  `mapstructure:"limit"`
+		Page       int64  `mapstructure:"page"`
+		PageCount  int64  `mapstructure:"page_count"`
+		UsersCount int64  `mapstructure:"users_count"`
+		Users      []User `mapstructure:"users"`
 	}
 )
 
 /********** METHODS **********/
 
-func (u *User) do(method, url, data string, queryParams []string, result interface{}) ([]byte, error) {
-	var body []byte
+func (u *User) do(method, url, data string, queryParams []string) (map[string]interface{}, error) {
+	var response []byte
 	var err error
 
 	u.request = u.request.updateRequest(u.request.clientID, u.request.clientSecret, u.request.fingerprint, u.request.ipAddress, u.AuthKey)
 
 	switch method {
 	case "GET":
-		body, err = u.request.Get(url, queryParams, result)
+		response, err = u.request.Get(url, queryParams)
 
 	case "POST":
-		body, err = u.request.Post(url, data, queryParams, result)
+		response, err = u.request.Post(url, data, queryParams)
 
 	case "PATCH":
-		body, err = u.request.Patch(url, data, queryParams, result)
+		response, err = u.request.Patch(url, data, queryParams)
 
 	case "DELETE":
-		body, err = u.request.Delete(url, result)
+		response, err = u.request.Delete(url)
 	}
 
 	switch err.(type) {
@@ -74,7 +76,7 @@ func (u *User) do(method, url, data string, queryParams []string, result interfa
 
 		u.request.authKey = u.AuthKey
 
-		return u.do(method, url, data, queryParams, result)
+		return u.do(method, url, data, queryParams)
 
 	case *IncorrectValues:
 		_, err := u.GetRefreshToken()
@@ -91,350 +93,242 @@ func (u *User) do(method, url, data string, queryParams []string, result interfa
 
 		u.request.authKey = u.AuthKey
 
-		return u.do(method, url, data, queryParams, result)
+		return u.do(method, url, data, queryParams)
 	}
 
-	return body, err
+	return read(response), err
 }
 
 /********** AUTHENTICATION **********/
 
 // Authenticate returns an oauth key and sets it to the user object
 func (u *User) Authenticate(data string) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(authURL, u.UserID)
 
-	_, err := u.do("POST", url, data, nil, &response)
+	res, err := u.do("POST", url, data, nil)
 
-	if err != nil {
-		return nil, err
+	if res["oauth_key"] != nil {
+		u.AuthKey = res["oauth_key"].(string)
+		u.request.authKey = res["oauth_key"].(string)
 	}
 
-	u.AuthKey = response["oauth_key"].(string)
-	u.request.authKey = response["oauth_key"].(string)
-
-	return response, err
+	return res, err
 }
 
 // GetRefreshToken performs a GET request and returns a new refresh token
-func (u *User) GetRefreshToken() (*Refresh, error) {
-	var refresh Refresh
-
+func (u *User) GetRefreshToken() (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID)
 
-	_, err := u.do("GET", url, "", nil, &refresh)
+	res, err := u.do("GET", url, "", nil)
 
-	if err != nil {
-		return nil, err
+	if res["refresh_token"] != nil {
+		u.RefreshToken = res["refresh_token"].(string)
 	}
 
-	u.RefreshToken = refresh.Token
-
-	return &refresh, err
+	return res, err
 }
 
 /********** NODE **********/
 
 // GetNodes returns all of the nodes associated with a user
-func (u *User) GetNodes(queryParams ...string) (*Nodes, error) {
-	var nodes Nodes
-
+func (u *User) GetNodes(queryParams ...string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"])
 
-	_, err := u.do("GET", url, "", nil, &nodes)
-
-	return &nodes, err
+	return u.do("GET", url, "", nil)
 }
 
 // GetNode returns a single node object
-func (u *User) GetNode(nodeID string, queryParams ...string) (*Node, error) {
-	var node Node
+func (u *User) GetNode(nodeID string, queryParams ...string) (map[string]interface{}, error) {
 
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID)
 
-	_, err := u.do("GET", url, "", nil, &node)
+	res, err := u.do("GET", url, "", nil)
 
-	return &node, err
+	return res, err
 }
 
 // CreateNode creates a node depending on the type of node specified
-func (u *User) CreateNode(data string) (*Nodes, error) {
-	var nodes Nodes
-
+func (u *User) CreateNode(data string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"])
 
-	_, err := u.do("POST", url, data, nil, &nodes)
-
-	return &nodes, err
+	return u.do("POST", url, data, nil)
 }
 
 // UpdateNode updates a node
-func (u *User) UpdateNode(nodeID, data string) (*Node, error) {
-	var node Node
+func (u *User) UpdateNode(nodeID, data string) (map[string]interface{}, error) {
 
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID)
 
-	_, err := u.do("PATCH", url, data, nil, &node)
-
-	return &node, err
+	return u.do("PATCH", url, data, nil)
 }
 
 // DeleteNode deletes a node
 func (u *User) DeleteNode(nodeID string) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID)
 
-	_, err := u.do("DELETE", url, "", nil, &response)
-
-	return response, err
+	return u.do("DELETE", url, "", nil)
 }
 
 /********** NODE (OTHER) **********/
 
 // AnswerMFA submits an answer to a MFA question from bank login attempt
-func (u *User) AnswerMFA(data string) (*Nodes, error) {
-	var nodes Nodes
-
+func (u *User) AnswerMFA(data string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"])
 
-	_, err := u.do("POST", url, data, nil, &nodes)
-
-	return &nodes, err
+	return u.do("POST", url, data, nil)
 }
 
 // GetApplePayToken generates tokenized info for Apple Wallet
 func (u *User) GetApplePayToken(nodeID, data string) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, "applepay")
 
-	_, err := u.do("PATCH", url, data, nil, &response)
-
-	return response, err
+	return u.do("PATCH", url, data, nil)
 }
 
 // ReinitiateMicroDeposit reinitiates micro-deposits for an ACH-US node with AC/RT
-func (u *User) ReinitiateMicroDeposit(nodeID string) (*Node, error) {
-	var node Node
-
+func (u *User) ReinitiateMicroDeposit(nodeID string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID) + "?resend_micro=YES"
 
-	_, err := u.do("PATCH", url, "", nil, &node)
-
-	return &node, err
+	return u.do("PATCH", url, "", nil)
 }
 
 // ResetDebitCard resets the debit card number, card cvv, and expiration date
 func (u *User) ResetDebitCard(nodeID string) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID) + "?reset=YES"
 
-	_, err := u.do("PATCH", url, "", nil, &response)
-
-	return response, err
+	return u.do("PATCH", url, "", nil)
 }
 
 // ShipDebitCard ships a physical debit card out to the user
 func (u *User) ShipDebitCard(nodeID, data string) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID) + "?ship=YES"
 
-	_, err := u.do("PATCH", url, data, nil, &response)
-
-	return response, err
+	return u.do("PATCH", url, data, nil)
 }
 
 // TriggerDummyTransactions triggers external dummy transactions on deposit or card accounts
 func (u *User) TriggerDummyTransactions(nodeID string, credit bool) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID) + "/dummy-tran"
 
 	if credit == true {
 		url += "?is_credit=YES"
 	}
 
-	_, err := u.do("GET", url, "", nil, &response)
-
-	return response, err
+	return u.do("GET", url, "", nil)
 }
 
 // VerifyMicroDeposit verifies micro-deposit amounts for a node
-func (u *User) VerifyMicroDeposit(nodeID, data string) (*Node, error) {
-	var node Node
-
+func (u *User) VerifyMicroDeposit(nodeID, data string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID)
 
-	body, err := u.do("PATCH", url, data, nil, &node)
-
-	node.Response = read(body)
-
-	return &node, err
+	return u.do("PATCH", url, data, nil)
 }
 
 /********** STATEMENT **********/
 
 // GetNodeStatements gets all of the node statements
-func (u *User) GetNodeStatements(nodeID string, queryParams ...string) (*Statements, error) {
-	var statements Statements
-
+func (u *User) GetNodeStatements(nodeID string, queryParams ...string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["statements"])
 
-	_, err := u.do("GET", url, "", nil, &statements)
-
-	return &statements, err
+	return u.do("GET", url, "", nil)
 }
 
 // GetStatements gets all of the user statements
-func (u *User) GetStatements(queryParams ...string) (*Statements, error) {
-	var statements Statements
-
+func (u *User) GetStatements(queryParams ...string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["statements"])
 
-	_, err := u.do("GET", url, "", nil, &statements)
-
-	return &statements, err
+	return u.do("GET", url, "", nil)
 }
 
 /********** SUBNET **********/
 
 // GetSubnets gets a single subnet object
-func (u *User) GetSubnets(nodeID string) (*Subnets, error) {
-	var subnets Subnets
-
+func (u *User) GetSubnets(nodeID string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["subnets"])
 
-	_, err := u.do("GET", url, "", nil, &subnets)
-
-	return &subnets, err
+	return u.do("GET", url, "", nil)
 }
 
 // GetSubnet gets a single subnet object
-func (u *User) GetSubnet(nodeID, subnetID string) (*Subnet, error) {
-	var subnet Subnet
-
+func (u *User) GetSubnet(nodeID, subnetID string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["subnets"], subnetID)
 
-	body, err := u.do("GET", url, "", nil, &subnet)
-
-	subnet.Response = read(body)
-
-	return &subnet, err
+	return u.do("GET", url, "", nil)
 }
 
 // CreateSubnet creates a subnet object
-func (u *User) CreateSubnet(nodeID, data string) (*Subnet, error) {
-	var subnet Subnet
-
+func (u *User) CreateSubnet(nodeID, data string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["subnets"])
 
-	body, err := u.do("PATCH", url, data, nil, &subnet)
-
-	subnet.Response = read(body)
-
-	return &subnet, err
+	return u.do("PATCH", url, data, nil)
 }
 
 /********** TRANSACTION **********/
 
 // GetTransactions returns transactions associated with a node
-func (u *User) GetTransactions(nodeID, transactionID string) (*Transactions, error) {
-	var transactions Transactions
-
+func (u *User) GetTransactions(nodeID, transactionID string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["trans"])
 
-	_, err := u.do("GET", url, "", nil, &transactions)
-
-	return &transactions, err
+	return u.do("GET", url, "", nil)
 }
 
 // GetTransaction returns a specific transaction associated with a node
-func (u *User) GetTransaction(nodeID, transactionID string) (*Transaction, error) {
-	var transaction Transaction
-
+func (u *User) GetTransaction(nodeID, transactionID string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["trans"], transactionID)
 
-	_, err := u.do("GET", url, "", nil, &transaction)
-
-	return &transaction, err
+	return u.do("GET", url, "", nil)
 }
 
 // CreateTransaction creates a transaction for the specified node
-func (u *User) CreateTransaction(nodeID, transactionID, data string) (*Transaction, error) {
-	var transaction Transaction
-
+func (u *User) CreateTransaction(nodeID, transactionID, data string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["trans"], transactionID)
 
-	body, err := u.do("POST", url, data, nil, &transaction)
-
-	transaction.Response = read(body)
-
-	return &transaction, err
+	return u.do("POST", url, data, nil)
 }
 
 // DeleteTransaction deletes/cancels a transaction
-func (u *User) DeleteTransaction(nodeID, transactionID string) (*Transaction, error) {
-	var transaction Transaction
-
+func (u *User) DeleteTransaction(nodeID, transactionID string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["transactions"], transactionID)
 
-	_, err := u.do("DELETE", url, "", nil, &transaction)
-
-	return &transaction, err
+	return u.do("DELETE", url, "", nil)
 }
 
 // CommentOnTransactionStatus adds comment to the transaction status
-func (u *User) CommentOnTransactionStatus(nodeID, transactionID, data string) (*Transaction, error) {
-	var transaction Transaction
-
+func (u *User) CommentOnTransactionStatus(nodeID, transactionID, data string) (map[string]interface{}, error) {
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["transactions"], transactionID)
 
-	_, err := u.do("POST", url, data, nil, &transaction)
-
-	return &transaction, err
+	return u.do("POST", url, data, nil)
 }
 
 // DisputeTransaction disputes a transaction for a user
 func (u *User) DisputeTransaction(nodeID, transactionID string) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(usersURL, u.UserID, path["nodes"], nodeID, path["transactions"], transactionID, "dispute")
 
 	data := string(`{
 		"dispute_reason":"CHARGE_BACK"
 	}`)
 
-	_, err := u.do("PATCH", url, data, nil, &response)
-
-	return response, err
+	return u.do("PATCH", url, data, nil)
 }
 
 /********** USER **********/
 
 // Update updates a single user and returns the updated user information
 func (u *User) Update(data string, queryParams ...string) (*User, error) {
-	var user User
-
 	url := buildURL(usersURL, u.UserID)
 
-	body, err := u.do("PATCH", url, data, nil, &user)
+	res, err := u.do("PATCH", url, data, nil)
 
-	user.Response = read(body)
+	mapstructure.Decode(res, u)
 
-	return &user, err
+	u.Response = res
+
+	return u, err
 }
 
 // CreateUBO creates and uploads an Ultimate Beneficial Ownership (UBO) and REG GG form as a physical document under the Business’s base document
 func (u *User) CreateUBO(data string) (map[string]interface{}, error) {
-	var response map[string]interface{}
-
 	url := buildURL(usersURL, u.UserID, "ubo")
 
-	_, err := u.do("PATCH", url, data, nil, &response)
-
-	return response, err
+	return u.do("PATCH", url, data, nil)
 }
