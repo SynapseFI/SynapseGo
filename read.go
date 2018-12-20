47aa734137fa5c308db1139d606fa7379ebce770
Nathaniel Niosco <nathan@synapsefi.com>
2018-12-19 16:30:59 -0800

    added comments

--- read.go
+++ read.go
@@ -1,11 +1,14 @@
 package synapse
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 func read(data []byte) map[string]interface{} {
 	d := make(map[string]interface{})
 	err := json.Unmarshal(data, &d)
 
+	// if data is an empty stream this will cause an unmarshal error
 	if err != nil {
 		panic(err)
 	}
