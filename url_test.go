ecf98b25f042e7f564f54dec36fd483eb144a0b8
Nathaniel Niosco <nathan@synapsefi.com>
2019-01-22 18:00:27 -0800

    updated tests

--- url_test.go
+++ url_test.go
@@ -9,13 +9,13 @@ import (
 /********** TESTS **********/
 
 func Test_BuildURL(t *testing.T) {
-	basePath := "https://randomsite.com"
+	basePath := "https://uat-api.synapsefi.com/v3.1"
 	path1 := "test1"
 	path2 := "test2"
 	path3 := "test3"
 	fullURL := basePath + "/" + path1 + "/" + path2 + "/" + path3
 
-	testURL := buildURL(basePath, path1, path2, path3)
+	testURL := buildURL(path1, path2, path3)
 
 	assert.Equal(t, fullURL, testURL)
 }
